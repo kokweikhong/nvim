c944417e4876d07e0156f893ccdb8db505b9063e
kokweikhong <kokweikhong@gmail.com>
2023-07-07 21:59:34 +0800

    change color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-  'folke/tokyonight.nvim',
-  branch = 'main',
-  priority = 1000,
+  -- 'folke/tokyonight.nvim',
+  -- branch = 'main',
+  -- priority = 1000,
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
   config = function()
-    vim.cmd.colorscheme 'tokyonight'
+    -- vim.cmd.colorscheme 'tokyonight'
+    vim.cmd.colorscheme "onedark"
   end,
 }
